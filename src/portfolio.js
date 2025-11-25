ba01d0c027a2f034bbeb5f418f945dae2527471b
Paco-Humbert <humbert.paco@gmail.com>
2025-11-25 09:12:12 +0100

    rename title html

--- src/portfolio.js
+++ src/portfolio.js
@@ -21,7 +21,7 @@ const projects = [
   {
     name: 'Argent Bank',
     description:
-      'Implémentez le front-end dans une application bancaire avec React',
+      'Implemented the front-end of an online banking application using React.',
     stack: ['SASS', 'React', 'JavaScript', 'Redux'],
     sourceCode: 'https://github.com/Paco-Humbert/10_HUMBERT_PACO_ARGENTBANK_09_2025',
     livePreview: 'https://github.com',
@@ -30,43 +30,43 @@ const projects = [
   {
     name: 'KASA',
     description:
-      'Créez une application web de location immobilière avec React',
+      'Created a real-estate rental web application using React.',
     stack: ['SASS', 'React'],
     sourceCode: 'https://github.com/Paco-Humbert/7_Humbert_Paco_KASA_062025',
-    livePreview: 'https://github.com',
+    livePreview: 'https://7-humbert-paco-kasa-062025.vercel.app/',
     
   },
   {
     name: 'Architecte Sophie Bluel',
     description:
-      'Créer une page web dynamique avec JavaScript',
+      'Developed a dynamic portfolio page for an architect using JavaScript.',
     stack: ['SASS', 'JavaScript'],
     sourceCode: 'https://github.com/Paco-Humbert/6_Humbert_Paco_architecte-sophie-bluel_052025',
-    livePreview: 'https://github.com',
+    livePreview: 'https://6-humbert-paco-architecte-sophie-bl-zeta.vercel.app/',
   },
   {
     name: 'Nina Carducci',
     description:
-      'Optimisation et débuggage du site de la photographe professionnelle Nina Carducci',
+      'Optimized and debugged the photography website of professional photographer Nina Carducci.',
     stack: ['SASS', 'JavaScript', 'SEO', 'Accessibility'],
     sourceCode: 'https://github.com/Paco-Humbert/8_Humbert_Paco_Nina_Carducci_072025',
-    livePreview: 'https://github.com',
+    livePreview: 'https://paco-humbert.github.io/8_Humbert_Paco_Nina_Carducci_072025/',
   },
   {
     name: '724 Events',
     description:
-      'Installez React Developer Tools, réparez les bugs. Rédigez le cahier de recette',
+      'Installed React Developer Tools, fixed multiple bugs, and wrote the full testing and recipe documentation (BDD/TDD).',
     stack: ['Debugging', 'Unit Testing', 'JavaScript'],
     sourceCode: 'https://github.com/Paco-Humbert/9_HUMBERT_PACO_724Events_082025',
-    livePreview: 'https://github.com',
+    livePreview: 'https://9-humbert-paco-724-events-082025.vercel.app/',
   },
   {
-    name: 'OH My Food',
+    name: 'Oh My Food',
     description:
-      'Développer un site “mobile first” qui répertorie les menus de restaurants gastronomiques.',
+      'Developed a mobile-first website showcasing menus from gourmet restaurants.',
     stack: ['Responsive Design','SASS','HTML','Figma'],
     sourceCode: 'https://github.com/Paco-Humbert/3_HUMBERT_PACO_OHMYFOOD_042025',
-    livePreview: 'https://github.com',
+    livePreview: 'https://paco-humbert.github.io/3_HUMBERT_PACO_OHMYFOOD_042025/',
   },
 ]
 
